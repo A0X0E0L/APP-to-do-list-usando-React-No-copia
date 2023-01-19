e9fea657508ca568a4b0fe07707309c6cdfc7f22
A0X0E0L <ax13xxi@gmail.com>
2023-01-19 13:37:37 +0000

    Terminacion TO DOdefinitiva LIST con React input controlado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -28,31 +28,36 @@ const Home = () => {
 	console.log(listaTareas);
 
 
+	const Borrar = (id)=>{
+setlistaTareas(listaTareas.filter((item) => item.id !== id));
+
+	}
+
+
 
 	
 	return (
 		
 		<>
+
+		<h1 className="d-flex justify-content-center" >To Do List</h1>
 		<form className="container" onSubmit={handleSubmit}>
   <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+    
     {/*2. definimos el evento ochange en el input */}
 	<input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(e)=>{setTarea(e.target.value)}}/>
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
   </div>
   
   <button type="submit" className="btn btn-primary">Submit</button>
 {/* item sería cualquier dato, puede ser index también */}
 <ul>
 	
-	{listaTareas.map((item) => (<li key={item.id}> {item.name}</li>))}
+	{listaTareas.map((item) => (<li key={item.id}> {item.name}<button type="button" className="btn btn-outline-light" onClick={(()=>Borrar(item.id))} >X</button></li>))}
 	
 </ul>
 
-{/* <button onClick={clickBorrar}
-                    className="btn-group btn-group-lg"
-                    type="button"
-                    id="button-addon1">Eliminar</button> */}
+
+
 </form>
 
 <div>
